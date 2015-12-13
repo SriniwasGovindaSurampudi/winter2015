1c6d9994748afe0e1a8b666a7a1d0f81c5a6b187
Abbhinav Venkat <abbhinavvenkat@Abbhinavs-Macbook-Pro.local>
2015-12-14 02:59:38 +0530

    Solved symmetry issue of L & Cf through rounding off & increased readability of code

--- Code/func_to_struct.m
+++ Code/func_to_struct.m
@@ -1,31 +1,58 @@
 %{
+Model Inversion: 
 Estimating Structural Connectivity given functional connectivity and degree
 of structural graph
+
 Data: Autism TD(Typically Developing i.e, Healthy samples)
 Age Group: 4 to 20.
 ROI: 264
 source: http://umcd.humanconnectomeproject.org
 Authors: Abbhinav Venkat, Govinda Sriniwas Surampudi
-Date: 12/12/2015
+Creation Date: 12/12/2015
 %}
 
+%*************************************************************************%
 %SVD
+
 [U, S, V] = svd(H_s2{t});
 sigma = sum(S, 2);
-[m, n] = find(sigma<sigma(1)*0.1);
-sigma(m) = 0;                       %Eliminating values < 0.1*S(1,1)
+[m, n] = find(sigma<sigma(1)*0.1);          %Choosing 90% of max value
+sigma(m) = 0;                               %Eliminating values < 0.1*S(1,1)
 S = diag(sigma);
 
+%*************************************************************************%
 %Re-computing the Functional Connectivity
+
 Cf = U*S*V';
+Cf = round(Cf, 7);
+symm_check = Cf - Cf';
+assert(isempty(find(symm_check~=0, 1)));    %To check for symmetry of Cf
 
+%Cf = H_s2{t};
+
+%We need to ensure that Cf is symmetric to get real eigen values
 [nvec, nval] = eig(Cf);
-a = sum(nval,2);
-
-gamma = -log(sum(nval,1))/t;
+gamma = -log(sum(nval,1))/t;                %!!!!!!!!COMPLEX
 
 %Re-computing L
 L = nvec*diag(gamma)*nvec';
 
 %Finding Structural Connectivity
 C = (D_s^0.5)*(diag(ones(size(W_s,1),1)) - L)*(D_s^0.5);
+
+%*************************************************************************%
+%Correlation between emerical and estimated structure 
+%{
+mean_calc = mean2(H_s2{i});
+mean_ground = mean2(Fc_norm);
+
+H = H_s2{i} - mean_calc;
+Fc_norm = Fc_norm - mean_ground;
+temp = H.*Fc_norm;
+H_sq = H.*H;
+Fc_sq = Fc_norm.*Fc_norm;
+
+pear_corr(i) = sum(temp(:))/(sqrt(sum(H_sq(:)))*sqrt(sum(Fc_sq(:))));
+fprintf('Per_corr for t=%d', i);
+disp(pear_corr(i));
+%}
--- Code/struct_to_func.m
+++ Code/struct_to_func.m
@@ -4,28 +4,45 @@ Data: Autism TD(Typically Developing i.e, Healthy samples)
 Age Group: 4 to 20.
 ROI: 264
 source: http://umcd.humanconnectomeproject.org
-Authors: Abbhinav Venkat, Govinda Surampudi
-Date: 11/12/2015
+Authors: Abbhinav Venkat, Govinda Sriniwas Surampudi
+Creation Date: 11/12/2015
 %}
 
+%***************************************************************************************************%
 %Reading data
 
 %Adjacency Matrix i.e, Structural Connectivity of size NxN
 W_s = dlmread('UCLA_Autism_TD128B_DTI_connectmat.txt');
-D_s = diag(sum(W_s, 1));                                       %D matrix
-
-%kk = (D_s^(-0.5))*W_s*(D_s^(-0.5));
-%LL = D_s - W_s;
+D_s = diag(sum(W_s, 2));                                    %Diagonal Degree matrix
 
 %Functional Connectivity of size NxN where N = #ROI
 FC = dlmread('UCLA_Autism_TD128_rsfMRI_connectmat.txt');    %Ground Truth of FC
 FC(isinf(FC)) = 0;                                          %Setting Diag as 0
 Fc_norm = (FC - min(FC(:)))/(max(FC(:)) - min(FC(:)));      %Normalizing in the range [0 1]
 
-%Calculating Laplacian sym
-%L_sym = (D_s^(-0.5))*LL*(D_s^(-0.5));
-L_sym = eye(size(W_s,1)) - (inv(D_s^(0.5)))*W_s*(inv(D_s^(0.5)));
+%***************************************************************************************************%
+%Calculating normalized weight matrix
 
+norm_W = ((D_s^(-0.5)))*W_s*((D_s^(-0.5)));
+norm_W = round(norm_W, 10);
+symm_check = norm_W - norm_W';
+assert(isempty(find(symm_check~=0, 1)));                       %To check for symmetry of norm_W
+
+%Calculating normalized symmetrix Laplacian
+L_sym = eye(size(W_s,1)) - norm_W;
+symm_check = L_sym - L_sym';
+assert(isempty(find(symm_check~=0, 1)));                       %To check for symmetry of L_sym
+
+%{
+symm_check = L_sym - L_sym';
+We notice that symm_check(i, j) = -symm_check(i, j)
+The values are of the order of 10^-17 which is very small. 
+Therefore, L_sym isn't symmetric. 
+This is because of a matlab round off error while calculating inverse
+So, we round it off to 10 decimal digits.
+%}
+
+%***************************************************************************************************%
 %Eig decomp. of lap.
 [vec, val] = eig(L_sym);
 
@@ -35,8 +52,12 @@ t = -1;
 H_s2 = cell(50, 1);
 pear_corr = size(50, 1);
 
-%exp(val*t) 
-%Running for different t from 1 to 50.
+%{
+exp(val*t) : Heat kernal
+Emperically found that Corr saturates as t tends to 50. 
+Running for different t from 1 to 50.
+%}
+
 for i = 1:50
     Diag_s_exp = diag(exp(-sum(val,1)*i));
     H_s = vec * Diag_s_exp * vec';                             %Heat Kernal
@@ -44,8 +65,11 @@ for i = 1:50
     %Normalizing in the range [0 1]
     H_s2{i} = (H_s - min(H_s(:)))/(max(H_s(:)) - min(H_s(:))); %Normalized Heat Kernal
 
-    %Pearson correlation between H_s2 and Fc_norm
-    %For drawing graph Corr(FC_ground_truth, FC_calculated) v/s Bt
+    %{
+    Pearson correlation between H_s2 and Fc_norm
+    For drawing graph Corr(FC_ground_truth, FC_calculated) v/s Bt
+    %}
+
     mean_calc = mean2(H_s2{i});
     mean_ground = mean2(Fc_norm);
 
@@ -67,7 +91,9 @@ for i = 1:50
     
 end
 
+%***************************************************************************************************%
 %Results
+
 fprintf('******----------------------------------******\n');
 fprintf('Max Pearson correlation = %d', maxi);
 fprintf('\nt = %d\n', t);
@@ -76,3 +102,5 @@ figure; imagesc(Fc_norm); title('Ground Truth of Functional Connectivity');
 figure; imagesc(H_s2{t}); title('Obtained Functional Connectivity');
 figure; plot(pear_corr);  title('Corr(FC ground truth, FC calculated) v/s Bt'); 
 xlabel('Bt'); ylabel('Correlation of FC calculated with empirical FC');
+
+%***************************************************************************************************%
